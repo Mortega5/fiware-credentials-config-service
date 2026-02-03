a1fa1de26d5179457177ed0e81055bcdca20cb20
Miguel Ortega Moreno <miguel.ortega@seamware.com>
2026-02-03 11:18:34 +0100

    fix(pagination): split service and scope queries to avoid paginating scopes (#26)

--- src/main/java/org/fiware/iam/rest/ServiceApiController.java
+++ src/main/java/org/fiware/iam/rest/ServiceApiController.java
@@ -12,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.fiware.iam.ServiceMapper;
 import org.fiware.iam.ccs.api.ServiceApi;
-import org.fiware.iam.ccs.model.*;
+import org.fiware.iam.ccs.model.CredentialVO;
+import org.fiware.iam.ccs.model.ServiceScopesEntryVO;
+import org.fiware.iam.ccs.model.ServiceVO;
+import org.fiware.iam.ccs.model.ServicesVO;
 import org.fiware.iam.exception.ConflictException;
 import org.fiware.iam.repository.ScopeEntry;
 import org.fiware.iam.repository.ScopeEntryRepository;
@@ -34,27 +37,27 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ServiceApiController implements ServiceApi {
 
-	private final ServiceRepository serviceRepository;
+    private final ServiceRepository serviceRepository;
     private final ScopeEntryRepository scopeEntryRepository;
-	private final ServiceMapper serviceMapper;
+    private final ServiceMapper serviceMapper;
 
-	@Override
-	public HttpResponse<Object> createService(@NonNull ServiceVO serviceVO) {
-		if (serviceVO.getId() != null && serviceRepository.existsById(serviceVO.getId())) {
-			throw new ConflictException(String.format("The service with id %s already exists.", serviceVO.getId()),
-					serviceVO.getId());
-		}
-		validateServiceVO(serviceVO);
+    @Override
+    public HttpResponse<Object> createService(@NonNull ServiceVO serviceVO) {
+        if (serviceVO.getId() != null && serviceRepository.existsById(serviceVO.getId())) {
+            throw new ConflictException(String.format("The service with id %s already exists.", serviceVO.getId()),
+                    serviceVO.getId());
+        }
+        validateServiceVO(serviceVO);
 
-		Service mappedService = serviceMapper.map(serviceVO);
+        Service mappedService = serviceMapper.map(serviceVO);
 
-		Service savedService = serviceRepository.save(mappedService);
+        Service savedService = serviceRepository.save(mappedService);
 
-		return HttpResponse.created(
-				URI.create(
-						ServiceApi.PATH_GET_SERVICE.replace(
-								"{id}", savedService.getId())));
-	}
+        return HttpResponse.created(
+                URI.create(
+                        ServiceApi.PATH_GET_SERVICE.replace(
+                                "{id}", savedService.getId())));
+    }
 
     @Transactional
     @Override
@@ -71,57 +74,68 @@ public class ServiceApiController implements ServiceApi {
         return HttpResponse.noContent();
     }
 
-	@Override
-	public HttpResponse<List<String>> getScopeForService(@NonNull String id, @Nullable String oidcScope) {
-		Optional<Service> service = serviceRepository.findById(id);
-		if (service.isEmpty()) {
-			return HttpResponse.notFound();
-		}
-		String selectedOidcScope =
-				oidcScope == null ?
-						serviceMapper.map(service.get()).getDefaultOidcScope() :
-						oidcScope;
-		ServiceScopesEntryVO serviceScopesEntryVO = serviceMapper.map(service.get())
-				.getOidcScopes()
-				.get(selectedOidcScope);
-		return HttpResponse.ok(
-				Optional.ofNullable(
-								serviceScopesEntryVO.getCredentials())
-						.orElse(List.of()).stream().map(CredentialVO::getType).toList());
-	}
+    @Override
+    public HttpResponse<List<String>> getScopeForService(@NonNull String id, @Nullable String oidcScope) {
+        Optional<Service> service = serviceRepository.findById(id);
+        if (service.isEmpty()) {
+            return HttpResponse.notFound();
+        }
+        String selectedOidcScope =
+                oidcScope == null ?
+                        serviceMapper.map(service.get()).getDefaultOidcScope() :
+                        oidcScope;
+        ServiceScopesEntryVO serviceScopesEntryVO = serviceMapper.map(service.get())
+                .getOidcScopes()
+                .get(selectedOidcScope);
+        return HttpResponse.ok(
+                Optional.ofNullable(
+                                serviceScopesEntryVO.getCredentials())
+                        .orElse(List.of()).stream().map(CredentialVO::getType).toList());
+    }
 
-	@Override
-	public HttpResponse<ServiceVO> getService(@NonNull String id) {
-		Optional<ServiceVO> serviceVO = serviceRepository.findById(id)
-				.map(serviceMapper::map);
-		return serviceVO
-				.map(HttpResponse::ok)
-				.orElse(HttpResponse.notFound());
-	}
+    @Override
+    public HttpResponse<ServiceVO> getService(@NonNull String id) {
+        Optional<ServiceVO> serviceVO = serviceRepository.findById(id)
+                .map(serviceMapper::map);
+        return serviceVO
+                .map(HttpResponse::ok)
+                .orElse(HttpResponse.notFound());
+    }
 
-	@Override
-	public HttpResponse<ServicesVO> getServices(@Nullable Integer nullablePageSize,
-												@Nullable Integer nullablePage) {
-		var pageSize = Optional.ofNullable(nullablePageSize).orElse(100);
-		var page = Optional.ofNullable(nullablePage).orElse(0);
-		if (pageSize < 1) {
-			throw new IllegalArgumentException("PageSize has to be at least 1.");
-		}
-		if (page < 0) {
-			throw new IllegalArgumentException("Offsets below 0 are not supported.");
-		}
+    @Override
+    public HttpResponse<ServicesVO> getServices(@Nullable Integer nullablePageSize,
+                                                @Nullable Integer nullablePage) {
+        var pageSize = Optional.ofNullable(nullablePageSize).orElse(100);
+        var page = Optional.ofNullable(nullablePage).orElse(0);
+        if (pageSize < 1) {
+            throw new IllegalArgumentException("PageSize has to be at least 1.");
+        }
+        if (page < 0) {
+            throw new IllegalArgumentException("Offsets below 0 are not supported.");
+        }
 
-		Page<Service> requestedPage = serviceRepository.findAll(
-				Pageable.from(page, pageSize, Sort.of(Sort.Order.asc("id"))));
-		return HttpResponse.ok(
-				new ServicesVO()
-						.total((int) requestedPage.getTotalSize())
-						.pageNumber(page)
-						.pageSize(requestedPage.getContent().size())
-						.services(requestedPage.getContent().stream().map(serviceMapper::map).toList()));
-	}
+        Page<Service> requestedPage = serviceRepository.findAll(
+                Pageable.from(page, pageSize, Sort.of(Sort.Order.asc("id"))));
+        List<Service> services = requestedPage.getContent();
 
-@Transactional
+        if (!services.isEmpty()) {
+            List<String> serviceIds = requestedPage.getContent().stream()
+                    .map(Service::getId)
+                    .toList();
+            List<ScopeEntry> allScopes = serviceRepository.findScopesByServiceIds(serviceIds);
+            Map<String, List<ScopeEntry>> scopesByServiceId = allScopes.stream()
+                    .collect(Collectors.groupingBy(scope -> scope.getService().getId()));
+            services.forEach(service -> service.setOidcScopes(scopesByServiceId.get(service.getId())));
+        }
+        return HttpResponse.ok(
+                new ServicesVO()
+                        .total((int) requestedPage.getTotalSize())
+                        .pageNumber(page)
+                        .pageSize(services.size())
+                        .services(services.stream().map(serviceMapper::map).toList()));
+    }
+
+    @Transactional
     @Override
     public HttpResponse<ServiceVO> updateService(@NonNull String id, @NonNull ServiceVO serviceVO) {
         if (serviceVO.getId() != null && !id.equals(serviceVO.getId())) {
@@ -143,85 +157,85 @@ public class ServiceApiController implements ServiceApi {
         Service toBeUpdated = serviceMapper.map(serviceVO);
         toBeUpdated.setId(id);
         Service updatedService = serviceRepository.update(toBeUpdated);
-        
+
         return HttpResponse.ok(serviceMapper.map(updatedService));
     }
 
-	// validate a service vo, e.g. check forbidden null values
-	private void validateServiceVO(ServiceVO serviceVO) {
-		if (serviceVO.getDefaultOidcScope() == null) {
-			throw new IllegalArgumentException("Default OIDC scope cannot be null.");
-		}
-		if (serviceVO.getOidcScopes() == null) {
-			throw new IllegalArgumentException("OIDC scopes cannot be null.");
-		}
+    // validate a service vo, e.g. check forbidden null values
+    private void validateServiceVO(ServiceVO serviceVO) {
+        if (serviceVO.getDefaultOidcScope() == null) {
+            throw new IllegalArgumentException("Default OIDC scope cannot be null.");
+        }
+        if (serviceVO.getOidcScopes() == null) {
+            throw new IllegalArgumentException("OIDC scopes cannot be null.");
+        }
 
-		String defaultOidcScope = serviceVO.getDefaultOidcScope();
-		ServiceScopesEntryVO serviceScopesEntryVO = serviceVO
-				.getOidcScopes()
-				.get(defaultOidcScope);
-		if (serviceScopesEntryVO == null) {
-			throw new IllegalArgumentException("Default OIDC scope must exist in OIDC scopes array.");
-		}
+        String defaultOidcScope = serviceVO.getDefaultOidcScope();
+        ServiceScopesEntryVO serviceScopesEntryVO = serviceVO
+                .getOidcScopes()
+                .get(defaultOidcScope);
+        if (serviceScopesEntryVO == null) {
+            throw new IllegalArgumentException("Default OIDC scope must exist in OIDC scopes array.");
+        }
 
-		Optional<CredentialVO> nullType = Optional.ofNullable(serviceScopesEntryVO
-						.getCredentials())
-				.orElse(List.of())
-				.stream()
-				.filter(cvo -> cvo.getType() == null)
-				.findFirst();
-		if (nullType.isPresent()) {
-			throw new IllegalArgumentException("Type of a credential cannot be null.");
-		}
+        Optional<CredentialVO> nullType = Optional.ofNullable(serviceScopesEntryVO
+                        .getCredentials())
+                .orElse(List.of())
+                .stream()
+                .filter(cvo -> cvo.getType() == null)
+                .findFirst();
+        if (nullType.isPresent()) {
+            throw new IllegalArgumentException("Type of a credential cannot be null.");
+        }
 
-		serviceVO
-				.getOidcScopes()
-				.values()
-				.forEach(this::validateKeyMappings);
-	}
+        serviceVO
+                .getOidcScopes()
+                .values()
+                .forEach(this::validateKeyMappings);
+    }
 
-	private void validateKeyMappings(ServiceScopesEntryVO scopeEntry) {
-		if (scopeEntry.getFlatClaims()) {
-			List<String> includedKeys = Optional.ofNullable(scopeEntry.getCredentials())
-					.orElse(List.of())
-					.stream()
-					.filter(cvo -> cvo.getJwtInclusion().getEnabled())
-					.flatMap(credentialVO ->
-							Optional.ofNullable(credentialVO.getJwtInclusion()
-											.getClaimsToInclude())
-									.orElse(List.of())
-									.stream()
-									.map(claim -> {
-										if (claim.getNewKey() != null && !claim.getNewKey().isEmpty()) {
-											return claim.getNewKey();
-										}
-										return claim.getOriginalKey();
-									})
-					).toList();
-			if (includedKeys.size() != new HashSet(includedKeys).size()) {
-				throw new IllegalArgumentException("Configuration contains duplicate claim keys.");
-			}
-		} else {
-			Optional.ofNullable(scopeEntry.getCredentials())
-					.orElse(List.of())
-					.stream()
-					.filter(cvo -> cvo.getJwtInclusion().getEnabled())
-					.forEach(cvo -> {
-						List<String> claimKeys = Optional.ofNullable(cvo.getJwtInclusion()
-										.getClaimsToInclude())
-								.orElse(List.of())
-								.stream()
-								.map(claim -> {
-									if (claim.getNewKey() != null && !claim.getNewKey().isEmpty()) {
-										return claim.getNewKey();
-									}
-									return claim.getOriginalKey();
-								})
-								.toList();
-						if (claimKeys.size() != new HashSet(claimKeys).size()) {
-							throw new IllegalArgumentException("Configuration contains duplicate claim keys.");
-						}
-					});
-		}
-	}
+    private void validateKeyMappings(ServiceScopesEntryVO scopeEntry) {
+        if (scopeEntry.getFlatClaims()) {
+            List<String> includedKeys = Optional.ofNullable(scopeEntry.getCredentials())
+                    .orElse(List.of())
+                    .stream()
+                    .filter(cvo -> cvo.getJwtInclusion().getEnabled())
+                    .flatMap(credentialVO ->
+                            Optional.ofNullable(credentialVO.getJwtInclusion()
+                                            .getClaimsToInclude())
+                                    .orElse(List.of())
+                                    .stream()
+                                    .map(claim -> {
+                                        if (claim.getNewKey() != null && !claim.getNewKey().isEmpty()) {
+                                            return claim.getNewKey();
+                                        }
+                                        return claim.getOriginalKey();
+                                    })
+                    ).toList();
+            if (includedKeys.size() != new HashSet(includedKeys).size()) {
+                throw new IllegalArgumentException("Configuration contains duplicate claim keys.");
+            }
+        } else {
+            Optional.ofNullable(scopeEntry.getCredentials())
+                    .orElse(List.of())
+                    .stream()
+                    .filter(cvo -> cvo.getJwtInclusion().getEnabled())
+                    .forEach(cvo -> {
+                        List<String> claimKeys = Optional.ofNullable(cvo.getJwtInclusion()
+                                        .getClaimsToInclude())
+                                .orElse(List.of())
+                                .stream()
+                                .map(claim -> {
+                                    if (claim.getNewKey() != null && !claim.getNewKey().isEmpty()) {
+                                        return claim.getNewKey();
+                                    }
+                                    return claim.getOriginalKey();
+                                })
+                                .toList();
+                        if (claimKeys.size() != new HashSet(claimKeys).size()) {
+                            throw new IllegalArgumentException("Configuration contains duplicate claim keys.");
+                        }
+                    });
+        }
+    }
 }
