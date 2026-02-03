9ef087c89b43f62c418c0a231cba1da4eb18de81
Miguel Ortega <miguel.ortega@seamware.com>
2026-02-03 10:53:48 +0100

    fix(pagination): split service and scope queries to avoid paginating scopes

--- src/main/java/org/fiware/iam/repository/ServiceRepository.java
+++ src/main/java/org/fiware/iam/repository/ServiceRepository.java
@@ -2,10 +2,12 @@ package org.fiware.iam.repository;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.data.annotation.Join;
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import io.micronaut.data.repository.PageableRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,9 +15,12 @@ import java.util.Optional;
  */
 public interface ServiceRepository extends PageableRepository<Service, String> {
 
-	@Join(value = "oidcScopes", type = Join.Type.LEFT_FETCH)
-	Optional<Service> findById(@NonNull String id);
+    @Join(value = "oidcScopes", type = Join.Type.LEFT_FETCH)
+    Optional<Service> findById(@NonNull String id);
 
-	@Join(value = "oidcScopes", type = Join.Type.LEFT_FETCH)
-	@NonNull Page<Service> findAll(@NonNull Pageable pageable);
+    @NonNull
+    Page<Service> findAll(@NonNull Pageable pageable);
+
+    @Query("SELECT * FROM scope_entry WHERE service_id IN (:serviceIds)")
+    List<ScopeEntry> findScopesByServiceIds(List<String> serviceIds);
 }
